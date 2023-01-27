32e671dd6b817a63cdd0437a65c5d4b4b1190207
cristian contreras <cr.ontreras@gmail.com>
2023-01-27 15:12:50 -0500

    ready for deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html','./src/**/*{html, js}'],
+  content: ['./index.html','./src/**/*{html, js}'],
   darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
-        'sanFrancisco': "url('../img/san-francisco.jpg')",
+        'sanFrancisco': "url('./public/img/san-francisco.jpg')",
         'sanFranciscoDesktop': "url('../img/san-francisco-desktop.jpg')",
-        'yosemite': "url('./public/img/yosemite.jpg')",
+        'yosemite': "url('../img/yosemite.jpg')",
         'LA': "url('../img/la.jpg')",
         'seattle': "url('../img/seattle.jpg')",
         'newYork': "url('../img/new-york.jpg')",
